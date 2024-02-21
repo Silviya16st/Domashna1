97cf2e3b76817d11beea9b0f9750974c8a83ef46
Silviya16st <silviyas225@gmail.com>
2024-02-21 09:45:38 +0200

    Zadacha

--- ListEven/Program.cs
+++ ListEven/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace ListEven
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace ListEven
 {
     internal class Program
     {
@@ -6,6 +9,7 @@
         {
             List<int> list = Console.ReadLine().Split().Select(int.Parse).ToList();
             List<int> listEven = new List<int>();
+            List<int> listOdd = new List<int>();
             for (int i = 0; i < list.Count; i++)
             {
                 if (list[i]%2 == 0)
@@ -13,7 +17,39 @@
                     listEven.Add(list[i]);
                 }
             }
-            Console.WriteLine(string.Join(" ",listEven));
+            Console.WriteLine("Chetni" + string.Join(" ", GetEvenElement(list)));
+            Console.WriteLine("Nechetni" + string.Join(" ", GetOddElement(list)));
+
+
+
+                
+         
+            
         }
+        static List<int> GetEvenElement(List<int> inputs)
+        {
+            List<int> listEven = new List<int>();
+            for (int i = 0; i < inputs.Count; i++)
+            {
+                if (inputs[i] % 2 == 0)
+                {
+                    listEven.Add(inputs[i]);
+                }              
+            }
+            return listEven;
+        }
+        static List<int> GetOddElement (List<int> inputs)
+        {
+            List<int> listOdd = new List<int>();
+            for (int i = 0; i < inputs.Count;i++)
+            {
+                if (inputs[i] % 2 != 0)
+                {
+                    listOdd.Add(inputs[i]);
+                }
+            }
+            return listOdd;
+        }
+        
     }
 }
